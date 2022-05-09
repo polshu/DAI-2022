14d16cb136618f595593fc1f497a21319505dfdc
Pabl Ulman <pablo.ulman@kapsch.net>
2022-05-09 06:36:26 -0300

    app-01

---
+++ 01-Node/app-01/index.js
@@ -0,0 +1,9 @@
+//
+// Ingresar el codigo debajo.
+//
+console.log("Primer Aplicación");
+
+//let strCadena = 'comillas simples - aplicacion con Node JS';
+//console.log(strCadena);
+//console.log("comillas dobles - aplicacion con Node JS");
+

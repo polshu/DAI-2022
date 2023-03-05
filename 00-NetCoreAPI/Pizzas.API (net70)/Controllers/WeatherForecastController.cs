ae2a35573c954df4a409f83a2ab14f2dec9db670
Polshu <polshu@polshu.com.ar>
2023-03-05 14:44:36 -0300

    Update WeatherForecastController.cs

--- 00-NetCoreAPI/Pizzas.API (net70)/Controllers/WeatherForecastController.cs
+++ 00-NetCoreAPI/Pizzas.API (net70)/Controllers/WeatherForecastController.cs
@@ -20,6 +20,7 @@ public class WeatherForecastController : ControllerBase {
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        //http://localhost:5000/WeatherForecast
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),

41d9406186d0a353b911b631600b589fecb86502
Polshu <polshu@polshu.com.ar>
2023-03-28 08:33:13 -0300

    varios

--- 01-Node/app-01/index.js
+++
@@ -1,9 +0,0 @@
-//
-// Ingresar el codigo debajo.
-//
-console.log("Primer Aplicación");
-
-//let strCadena = 'comillas simples - aplicacion con Node JS';
-//console.log(strCadena);
-//console.log("comillas dobles - aplicacion con Node JS");
-
